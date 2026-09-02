20648cbca698eeaee4d30c93c07102e9703aee38
GUNAPATISANTHOSH <santoshsantutej@gmail.com>
2026-09-02 17:48:40 +0530

    project

---
+++ src/app/App.jsx
@@ -0,0 +1,8 @@
+import { RouterProvider } from 'react-router';
+import { AppProvider } from './context/AppContext';
+import { router } from './routes';
+export default function App() {
+    return (<AppProvider>
+      <RouterProvider router={router}/>
+    </AppProvider>);
+}
